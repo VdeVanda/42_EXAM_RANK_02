f7e3a48f78833ae7e8727df3641f9351f9d6c710
vdevanda <vabatist@student.42lisboa.com>
2025-02-15 18:23:43 +0000

    level 1 almost fully reviewed

--- level_01/rev_print.c
+++ level_01/rev_print.c
@@ -18,3 +18,20 @@ LIOP a 0bud$
 $> ./rev_print | cat -e
 $
 */
+
+#include <unistd.h>
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+        int	i;
+    	i = 0;
+		while (argv[1][i])
+			i++;
+		while (i--)
+			write(1, &argv[1][i], 1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
