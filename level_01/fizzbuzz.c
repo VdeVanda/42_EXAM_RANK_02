00ac9c3e79c616d3686662836b15d93cc97bf250
vdevanda <vabatist@student.42lisboa.com>
2025-02-18 13:37:13 +0000

    simplify fizzbuzz

--- level_01/fizzbuzz.c
+++ level_01/fizzbuzz.c
@@ -60,7 +60,7 @@ void	ft_write_number(int number)
 
 void	ft_write_number(int number)
 {
-	if (number > 9)
+	if (number >= 10)
 		ft_write_number(number / 10);
 	write(1, &"0123456789"[number % 10], 1);
 }
--- level_02/ft_atoi.c
+++ level_02/ft_atoi.c
@@ -1,4 +1,4 @@
-/* 
+/*
 Assignment name  : ft_atoi
 Expected files   : ft_atoi.c
 Allowed functions: None
@@ -16,5 +16,36 @@ int	ft_atoi(const char *str);
 
 int	ft_atoi(const char *str)
 {
-    
+	int	i;
+	int	sign;
+	int	number;
+
+	i = 0;
+	sign = 1;
+	number = 0;
+	while (str[i] <= 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		number = number * 10 + (str[i] - '0');
+		i++;
+	}
+	return (number * sign);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int	main(int argc, char **argv)
+// {
+// 	(void)argc;
+// 	printf("%d\n", ft_atoi(argv[1]));
+// 	printf("%d\n", atoi(argv[1]));
+// 	return (0);
+// }
