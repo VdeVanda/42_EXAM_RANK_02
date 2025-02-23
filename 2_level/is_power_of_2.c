9d81592374e694343408357168b3e775be0a5dd7
VdeVanda <vandaoliveir@gmail.com>
2025-02-23 22:22:39 +0000

    almost done level 2

---
+++ 2_level/is_power_of_2.c
@@ -0,0 +1,35 @@
+/*
+Assignment name  : is_power_of_2
+Expected files   : is_power_of_2.c
+Allowed functions: None
+--------------------------------------------------------------------------------
+
+Write a function that determines if a given number is a power of 2.
+
+This function returns 1 if the given number is a power of 2, otherwise it returns 0.
+
+Your function must be declared as follows:
+
+int	    is_power_of_2(unsigned int n);
+*/
+
+int	    is_power_of_2(unsigned int n)
+{	
+	int test;
+    test = 1;
+
+	while (test <= n)
+	{
+		if (test == n)
+			return  (1);
+		test = test * 2;
+	}
+	return (0);
+}
+
+
+#include <stdio.h>
+int main(void)
+{
+	printf("%d\n", is_power_of_2(1024));
+}
