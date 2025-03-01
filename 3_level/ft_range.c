439e0d644e112e3b7a4ed04f9d337a2fa1407e96
VdeVanda <127157914+VdeVanda@users.noreply.github.com>
2025-03-01 13:04:25 +0000

    some more level 3 done

---
+++ 3_level/ft_range.c
@@ -0,0 +1,90 @@
+/*
+Assignment name  : ft_range
+Expected files   : ft_range.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write the following function:
+
+int     *ft_range(int start, int end);
+
+It must allocate (with malloc()) an array of integers, fill it with consecutive
+values that begin at start and end at end (Including start and end !), then
+return a pointer to the first value of the array.
+
+Examples:
+
+- With (1, 3) you will return an array containing 1, 2 and 3.
+- With (-1, 2) you will return an array containing -1, 0, 1 and 2.
+- With (0, 0) you will return an array containing 0.
+- With (0, -3) you will return an array containing 0, -1, -2 and -3.
+*/
+
+#include <stdlib.h>
+
+int	*ft_range(int start, int end)
+{
+	int	len;
+	int	*arr;
+	int	i;
+
+	if (start > end)
+		len = start - end + 1;
+	else
+		len = end - start + 1;
+
+	arr = malloc(sizeof(int) * len);
+	if (!arr)
+		return (NULL);
+
+	i = 0;
+	if (start < end)
+	{
+		while (start <= end)
+		{
+			arr[i] = start;
+			start += 1;
+			i += 1;
+		}
+	}
+	else
+	{
+		while (start >= end)
+		{
+			arr[i] = start;
+			start -= 1;
+			i += 1;
+		}
+	}
+	return (arr);
+}
+/*
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Usage: %s <start> <end>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+	int	start = atoi(argv[1]);
+	int	end = atoi(argv[2]);
+	int	*arr = ft_range(start, end);
+	int	size;
+
+	if (start > end)
+		size = start - end + 1;
+	else
+		size = end - start + 1;
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	free(arr);
+	return (EXIT_SUCCESS);
+}
+*/
---
+++ 3_level/ft_rrange.c
@@ -0,0 +1,89 @@
+/*
+Assignment name  : ft_rrange
+Expected files   : ft_rrange.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write the following function:
+
+int     *ft_rrange(int start, int end);
+
+It must allocate (with malloc()) an array of integers, fill it with consecutive
+values that begin at end and end at start (Including start and end !), then
+return a pointer to the first value of the array.
+
+Examples:
+
+- With (1, 3) you will return an array containing 3, 2 and 1
+- With (-1, 2) you will return an array containing 2, 1, 0 and -1.
+- With (0, 0) you will return an array containing 0.
+- With (0, -3) you will return an array containing -3, -2, -1 and 0.
+*/
+
+#include <stdlib.h>
+
+int     *ft_rrange(int start, int end)
+
+{
+	int	len;
+	int	*arr;
+	int	i;
+
+	if (start > end)
+		len = start - end + 1;
+	else
+		len = end - start + 1;
+
+	arr = malloc(sizeof(int) * len);
+    if (!arr)
+	return (NULL);
+	i = 0;
+	if (start <= end)
+	{
+		while (end >= start)
+		{
+			arr[i] = end;
+			end--;
+            i++;
+		}
+    }
+	else
+    {
+			while (end <= start)
+            {
+                arr[i] = end;
+                end++;
+                i++;
+            }
+    }
+	return (arr);
+}
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Usage: %s <start> <end>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+	int	start = atoi(argv[1]);
+	int	end = atoi(argv[2]);
+	int	*arr = ft_rrange(start, end);
+	int	size;
+
+	if (start > end)
+		size = start - end + 1;
+	else
+		size = end - start + 1;
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	free(arr);
+	return (EXIT_SUCCESS);
+}
