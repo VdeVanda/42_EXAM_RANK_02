d8b5a2667ded15d129fb3cfa5769f81b31a6ac86
VdeVanda <127157914+VdeVanda@users.noreply.github.com>
2025-03-01 13:05:11 +0000

    atoi base fixed

--- 3_level/ft_atoi_base.c
+++ 3_level/ft_atoi_base.c
@@ -23,7 +23,18 @@ int	ft_atoi_base(const char *str, int str_base);
 
 #include <stdio.h>
 
-/* int	ft_atoi_base(const char *str, int str_base)
+int	get_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (c - '0');
+	else if (c >= 'A' && c <= 'F')
+		return (c - 'A' + 10);
+	else if (c >= 'a' && c <= 'f')
+		return (c - 'a' + 10);
+	return (0);
+}
+
+int	ft_atoi_base(const char *str, int str_base)
 {
 	int	i;
 	int	sign;
@@ -32,50 +43,6 @@ int	ft_atoi_base(const char *str, int str_base);
 	i = 0;
 	sign = 1;
 	result = 0;
-
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	while (str[i])
-	{
-		result *= str_base;
-		if (str[i] >= '0' && str[i] <= '9')
-			result += str[i] - '0';
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			result += str[i] - 55;
-		else if (str[i] >= 'a' && str[i] <= 'z')
-			result += str[i] - 87;
-		else
-			break;
-		i++;
-		//// Este atoi_base nao lida com espacos, pontos, virgulas...
-		//// Tem de ser revisto ---
-	}
-	return (result * sign);
-} */
-
-int	get_digit(char c)
-{
-	if (c >= '0' && c <= '9')
-		return (c - '0');
-	else if (c >= 'A' && c <= 'F')
-		return (c - 55);
-	else if (c >= 'a' && c <= 'f')
-		return (c - 87);
-	return (0);
-}
-
-int	ft_atoi_base(const char *str, int str_base)
-{
-	int	i;
-	int	sign;
-	int	res;
-
-	i = 0;
-	sign = 1;
-	res = 0;
 	while (str[i] == ' ' || str[i] == '\t')
 		i++;
 	if (str[i] == '-')
@@ -87,12 +54,12 @@ int	ft_atoi_base(const char *str, int str_base)
 	{
 		if (get_digit(str[i]))
 		{
-			res *= str_base;
-			res += get_digit(str[i]);
+			result *= str_base;
+			result += get_digit(str[i]);
 		}
 		i++;
 	}
-	return (res * sign);
+	return (result * sign);
 }
 
 int	main(void)
